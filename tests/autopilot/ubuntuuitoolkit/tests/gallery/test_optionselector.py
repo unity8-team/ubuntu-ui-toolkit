a5bad9d10f396b0f940c7256dd1ca20d4e4974eb
Nic <nicolas.doffay@canonical.com>
2014-01-10 14:39:34 +0000

    Reverted change.

--- tests/autopilot/ubuntuuitoolkit/tests/gallery/test_optionselector.py
+++ tests/autopilot/ubuntuuitoolkit/tests/gallery/test_optionselector.py
@@ -30,12 +30,12 @@ class OptionSelectorTests(GalleryTestCase):
         self.loadItem(item)
         self.checkPageHeader(item)
         collapsed = self.getObject("optionselector_collapsed")
-        standard = collapsed.select_single('Standard',
+        styleditem = collapsed.select_single('Standard',
                                              objectName='listContainer')
 
         self.assertThat(collapsed.selectedIndex, Equals(0))
         self.pointing_device.click_object(collapsed)
-        self.assertThat(standard.currentlyExpanded, Eventually(Equals(True)))
+        self.assertThat(styleditem.currentlyExpanded, Eventually(Equals(True)))
         #try to search the following list entry few times
         #as it may not be available immediately.
         for t in range(0, 9):
@@ -48,7 +48,7 @@ class OptionSelectorTests(GalleryTestCase):
 
         self.pointing_device.click_object(selectedValue)
         self.assertThat(collapsed.selectedIndex, Eventually(Equals(3)))
-        self.assertThat(standard.currentlyExpanded,
+        self.assertThat(styleditem.currentlyExpanded,
                         Eventually(Equals(False)))
 
     def test_optionselector_expanded(self):
