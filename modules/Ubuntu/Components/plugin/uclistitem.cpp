f0b7473970c93ec9c16f4416e9d0e52fe327b6d0
Zsombor Egri <zsombor.egri@canonical.com>
2014-12-17 16:17:35 +0200

    main headline in the documentation added

--- modules/Ubuntu/Components/plugin/uclistitem.cpp
+++ modules/Ubuntu/Components/plugin/uclistitem.cpp
@@ -904,6 +904,55 @@ void UCListItemPrivate::clampAndMoveX(qreal &x, qreal dx)
  * implies is that leading and trailing actions cannot be swiped in. \ selectable
  * property can be used to implement different behavior when \l clicked or \l
  * pressAndHold.
+ *
+ * \section3 Dragging mode
+ * The dragging mode is only supported on ListView, as it requires a model supported
+ * view to be used. The drag mode can be activated using the \l ListItem::dragMode
+ * attached property that is attached to the ListView. The items will show a panel
+ * as defined in the \l ListItemStyle::dragHandlerDelegate, and dragging will be
+ * possible only through that panel. Pressing or clicking anywhere else on the ListItem
+ * will invoke the item's action assigned to the touched area.
+ *
+ * The most important thing to remember when implementing dragging on a ListItem is to
+ * implement the \l ListItem::draggingUpdated signal, and to move the model data whenever
+ * desired. \l ListItem::draggingStarted signal implementation is only required if there
+ * are preconditions to be applied prior to initiate the dragging.
+ *
+ * ListItem does not provide animations when the ListView's model is updated. In order
+ * to have animation, use UbuntuListView or provide a transition animation to the
+ * moveDisplaced property of the ListView.
+ *
+ * \qml
+ * import QtQuick 2.3
+ * import Ubuntu.Components 1.2
+ *
+ * ListView {
+ *     model: ListModel {
+ *         Component.onCompleted: {
+ *             for (var i = 0; i < 100; i++) {
+ *                 append({tag: "List item #"+i});
+ *             }
+ *         }
+ *     }
+ *     delegate: ListItem {
+ *         Label {
+ *             text: modelData
+ *         }
+ *         color: draggable ? "lightblue" : "lightgray"
+ *         onPressAndHold: ListView.view.ListItem.dragMode = !ListView.view.ListItem.dragMode
+ *     }
+ *     ListItem.draggingUpdated: {
+ *         model.move(event.from, event.to, 1);
+ *     }
+ *     moveDisplaced: Transition {
+ *         UbuntuNumberAnimation {
+ *             property: "y"
+ *         }
+ *     }
+ * }
+ * \endqml
+ *
+ * \sa ListItem::dragMode, ListItem::draggingStarted, ListItem::draggingUpdated
  */
 
 /*!
