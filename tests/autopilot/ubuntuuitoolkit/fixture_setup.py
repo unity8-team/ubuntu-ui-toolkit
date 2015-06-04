c934c2687b97591cd16879858531679104d80b9e
Leonardo Arias Fonseca <leo.arias@canonical.com>
2015-06-04 05:37:07 -0600

    Removed the %u.

--- tests/autopilot/ubuntuuitoolkit/fixture_setup.py
+++ tests/autopilot/ubuntuuitoolkit/fixture_setup.py
@@ -46,7 +46,7 @@ MainView {
 DEFAULT_DESKTOP_FILE_DICT = {
     'Type': 'Application',
     'Name': 'test',
-    'Exec': '{qmlscene} {qml_file_path} %u',
+    'Exec': '{qmlscene} {qml_file_path}',
     'Icon': 'Not important'
 }
 
