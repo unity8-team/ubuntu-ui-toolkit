00e0c458b4cf32e7844056360efbf93afacfce82
Timo Jyrinki <timo.jyrinki@canonical.com>
2016-01-15 11:17:36 +0200

    bump

--- push_to_phone.sh
+++ push_to_phone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright 2014 Canonical Ltd.
-#
+# 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation; version 3.
